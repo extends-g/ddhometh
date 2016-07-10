75fbbb96ef42a02563d5a8099d1feffe17a57055
wannaporn <wannaporn@intbizth.com>
2016-07-10 17:32:02 +0700

    project crud complete :)

--- src/Dhome/Bundle/AdminBundle/Form/Type/ProjectType.php
+++ src/Dhome/Bundle/AdminBundle/Form/Type/ProjectType.php
@@ -13,8 +13,42 @@ class ProjectType extends AbstractResourceType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+            ->add('category', 'dhome_project_category_choice', [
+                'required' => true,
+                'choice_label' => 'name',
+                'empty_value' => 'Please select project category.',
+                'label' => 'Project category'
+            ])
+
             ->add('name', 'text', [
                 'label' => 'Name',
+                'max_length' => 4,
+                'required' => true
+            ])
+
+            ->add('shortDescription', 'text', [
+                'label' => 'Short description',
+                'required' => false
+            ])
+
+            ->add('content', 'textarea', [
+                'label' => 'content',
+                'attr' => array(
+                    'class' => 'tinymce',
+                )
+            ])
+
+            ->add('videoLink', 'url', [
+                'required' => false,
+                'label' => 'Video link'
+            ])
+
+            ->add('images', 'collection', [
+                'type' => 'dhome_project_image',
+                'allow_add' => true,
+                'allow_delete' => true,
+                'by_reference' => false,
+                'label' => 'Project image',
             ])
         ;
     }
--- src/Dhome/Bundle/MediaBundle/EventListener/ImageUploadListener.php
+++ src/Dhome/Bundle/MediaBundle/EventListener/ImageUploadListener.php
@@ -2,6 +2,7 @@
 
 namespace Dhome\Bundle\MediaBundle\EventListener;
 
+use Dhome\Bundle\AdminBundle\Model\ProjectInterface;
 use Dhome\Bundle\AdminBundle\Model\VisionInterface;
 use Dhome\Bundle\MediaBundle\Uploader\ImageUploaderInterface;
 use Symfony\Component\EventDispatcher\GenericEvent;
@@ -33,42 +34,16 @@ class ImageUploadListener
         $this->uploadVisionImages($subject);
     }
 
-//    /**
-//     * @param GenericEvent $event
-//     */
-//    public function uploadProductVariantImage(GenericEvent $event)
-//    {
-//        $subject = $event->getSubject();
-//        Assert::isInstanceOf($subject, ProductVariantInterface::class);
-//
-//        $this->uploadProductVariantImages($subject);
-//    }
-//
-//    /**
-//     * @param GenericEvent $event
-//     */
-//    public function uploadProductImage(GenericEvent $event)
-//    {
-//        $subject = $event->getSubject();
-//        Assert::isInstanceOf($subject, ProductInterface::class);
-//
-//        if ($subject->isSimple()) {
-//            $this->uploadProductVariantImages($subject->getFirstVariant());
-//        }
-//    }
-
     /**
      * @param GenericEvent $event
      */
-//    public function uploadTaxonImage(GenericEvent $event)
-//    {
-//        $subject = $event->getSubject();
-//        Assert::isInstanceOf($subject, TaxonInterface::class);
-//
-//        if ($subject->hasFile()) {
-//            $this->uploader->upload($subject);
-//        }
-//    }
+    public function uploadProjectImage(GenericEvent $event)
+    {
+        $subject = $event->getSubject();
+        Assert::isInstanceOf($subject, ProjectInterface::class);
+
+        $this->uploadProjectImages($subject);
+    }
 
     /**
      * @param VisionInterface $vision
@@ -76,6 +51,24 @@ class ImageUploadListener
     private function uploadVisionImages(VisionInterface $vision)
     {
         $images = $vision->getImages();
+        foreach ($images as $image) {
+            if ($image->hasFile()) {
+                $this->uploader->upload($image);
+            }
+
+            // Upload failed? Let's remove that image.
+            if (null === $image->getPath()) {
+                $images->removeElement($image);
+            }
+        }
+    }
+
+    /**
+     * @param ProjectInterface $project
+     */
+    private function uploadProjectImages(ProjectInterface $project)
+    {
+        $images = $project->getImages();
         foreach ($images as $image) {
             if ($image->hasFile()) {
                 $this->uploader->upload($image);
