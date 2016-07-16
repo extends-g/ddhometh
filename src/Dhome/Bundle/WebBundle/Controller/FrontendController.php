af53609eeb9aafd06dfa730921272aefe4685f81
KannipaS <sr.kannipa_st@tni.ac.th>
2016-07-16 20:00:54 +0700

    frontend recheck

--- src/Dhome/Bundle/WebBundle/Controller/FrontendController.php
+++ src/Dhome/Bundle/WebBundle/Controller/FrontendController.php
@@ -21,6 +21,16 @@ class FrontendController extends Controller
         return $this->render('DhomeWebBundle::web/project.html.twig');
     }
 
+    public function pressAction()
+    {
+        return $this->render('DhomeWebBundle::web/press.html.twig');
+    }
+
+    public function collectionAction()
+    {
+        return $this->render('DhomeWebBundle::web/collection.html.twig');
+    }
+
     public function inspirationAction()
     {
         return $this->render('DhomeWebBundle::web/inspiration.html.twig');
