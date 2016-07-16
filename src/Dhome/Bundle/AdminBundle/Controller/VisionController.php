c64a39cdc7bd8124deac7d30b171d4f9b013e38b
wannaporn <wannaporn@intbizth.com>
2016-07-16 19:12:59 +0700

    inspiration.

--- src/Dhome/Bundle/AdminBundle/Controller/VisionController.php
+++ src/Dhome/Bundle/AdminBundle/Controller/VisionController.php
@@ -6,5 +6,28 @@ use Sylius\Bundle\ResourceBundle\Controller\ResourceController;
 
 class VisionController extends ResourceController
 {
+    /**
+     * @return \Sylius\Bundle\ResourceBundle\Doctrine\ORM\EntityRepository
+     */
+    protected function getVisionRepository()
+    {
+        return $this->get('dhome.repository.vision');
+    }
 
+    public function webIndexAction($limit = 12, $currentPage = 1)
+    {
+        $visions = $this->getVisionRepository()->createPaginator([], ['o.createdAt' => 'desc']);
+        $visions->setMaxPerPage($limit);
+
+        return $this->render('DhomeWebBundle::web/inspiration.html.twig', [
+            'inspirations' => $visions,
+        ]);
+    }
+
+    public function webShowAction($id)
+    {
+        return $this->render('DhomeWebBundle::web/_inspiration-show.html.twig', [
+            'inspiration' => $this->getVisionRepository()->find($id),
+        ]);
+    }
 }
---
+++ src/Dhome/Bundle/AdminBundle/Doctrine/ORM/VisionRepository.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Dhome\Bundle\AdminBundle\Doctrine\ORM;
+
+use Sylius\Bundle\ResourceBundle\Doctrine\ORM\EntityRepository;
+
+class VisionRepository extends EntityRepository implements VisionRepositoryInterface
+{
+}
