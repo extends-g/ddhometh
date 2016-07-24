8396fa15df597636cfcff9971b7409d4ff72e422
wannaporn <wannaporn@intbizth.com>
2016-07-24 22:46:55 +0700

    user acl.

--- src/Dhome/Bundle/AdminBundle/Controller/PressController.php
+++ src/Dhome/Bundle/AdminBundle/Controller/PressController.php
@@ -2,9 +2,74 @@
 
 namespace Dhome\Bundle\AdminBundle\Controller;
 
+use Dhome\Bundle\AdminBundle\Model\PressInterface;
 use Sylius\Bundle\ResourceBundle\Controller\ResourceController;
+use Sylius\Component\Resource\ResourceActions;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use FOS\RestBundle\View\View;
+use Symfony\Component\Security\Core\Authentication\Token\RememberMeToken;
 
 class PressController extends ResourceController
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function createAction(Request $request)
+    {
+        $configuration = $this->requestConfigurationFactory->create($this->metadata, $request);
 
+        $this->isGrantedOr403($configuration, ResourceActions::CREATE);
+        $newResource = $this->newResourceFactory->create($configuration, $this->factory);
+
+        $form = $this->resourceFormFactory->create($configuration, $newResource);
+
+        if ($request->isMethod('POST') && $form->submit($request)->isValid()) {
+            /** @var PressInterface $newResource */
+            $newResource = $form->getData();
+
+            /** @var RememberMeToken $test */
+            $userToken = $this->get('security.token_storage')->getToken();
+            $newResource->setUser($userToken->getUser());
+
+            $event = $this->eventDispatcher->dispatchPreEvent(ResourceActions::CREATE, $configuration, $newResource);
+
+            if ($event->isStopped() && !$configuration->isHtmlRequest()) {
+                throw new HttpException($event->getErrorCode(), $event->getMessage());
+            }
+            if ($event->isStopped()) {
+                $this->flashHelper->addFlashFromEvent($configuration, $event);
+
+                return $this->redirectHandler->redirectToIndex($configuration, $newResource);
+            }
+
+            $this->repository->add($newResource);
+            $this->eventDispatcher->dispatchPostEvent(ResourceActions::CREATE, $configuration, $newResource);
+
+            if (!$configuration->isHtmlRequest()) {
+                return $this->viewHandler->handle($configuration, View::create($newResource, 201));
+            }
+
+            $this->flashHelper->addSuccessFlash($configuration, ResourceActions::CREATE, $newResource);
+
+            return $this->redirectHandler->redirectToResource($configuration, $newResource);
+        }
+
+        if (!$configuration->isHtmlRequest()) {
+            return $this->viewHandler->handle($configuration, View::create($form, 400));
+        }
+
+        $view = View::create()
+            ->setData([
+                'configuration' => $configuration,
+                'metadata' => $this->metadata,
+                'resource' => $newResource,
+                $this->metadata->getName() => $newResource,
+                'form' => $form->createView(),
+            ])
+            ->setTemplate($configuration->getTemplate(ResourceActions::CREATE . '.html'))
+        ;
+
+        return $this->viewHandler->handle($configuration, $view);
+    }
 }
